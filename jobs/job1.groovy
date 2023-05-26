a896bcf9bccafe26882c3bb57b61b2f6b4e5b146
oviner <oviner@redhat.com>
2023-05-26 20:30:14 +0300

    fix

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,27 +1,18 @@
 def job = job('job1') {
     displayName('job1')
-    definition {
-        cpsScm {
-            lightweight(lightweight = false)
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/OdedViner/Jenkins-pipline.git')
-                    }
-                    extensions {
-                        wipeOutWorkspace()
-                    }
-                    branch('main')
+    cpsScm {
+        lightweight(lightweight = false)
+        scm {
+            git {
+                remote {
+                    url('https://github.com/OdedViner/Jenkins-pipline.git')
                 }
+                extensions {
+                    wipeOutWorkspace()
+                }
+                branch('main')
             }
-            scriptPath('jobs/pipelines/job1/Jenkinsfile')
         }
-    }
-    // parameters {
-    //     parameters.cluster_name_param delegate
-    // }
-    // environmentVariables {
-    //     keepSystemVariables(true)
-    //     keepBuildVariables(true)
-    // }
+        scriptPath('jobs/pipelines/job1/Jenkinsfile')
+    }    
 }
