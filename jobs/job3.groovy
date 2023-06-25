cd04498784d9d7a65f24e4c9c9640308baccb65f
oviner <oviner@redhat.com>
2023-06-25 23:55:10 +0300

    fix code

--- jobs/job3.groovy
+++ jobs/job3.groovy
@@ -1,3 +1,24 @@
-def job = job('job3') {
+def job = pipelineJob('job3') {
     displayName('job3')
+    definition {
+        cpsScm {
+            lightweight(lightweight = false)
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/OdedViner/Jenkins-pipline.git')
+                    }
+                    extensions {
+                        wipeOutWorkspace()
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('jobs/pipelines/job3/Jenkinsfile')
+        }
+  
+    }
+    parameters {
+        stringParam('HOURS', '44', 'Maximum running time')
+    }
 }
