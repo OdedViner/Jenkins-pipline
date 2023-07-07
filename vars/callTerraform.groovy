bc2cb0915efece43c44cf049fda2953d6c141ecc
oviner <oviner@redhat.com>
2023-07-07 13:15:14 +0300

    fix code

--- vars/callTerraform.groovy
+++ vars/callTerraform.groovy
@@ -4,7 +4,7 @@ def call(tf_workspace) {
         sh "terraform init"
         sh "terraform apply --auto-approve"
       data['ip_address'] = sh (
-        script: """terraform output ip_address | tr -d '"'""",
+        script: """terraform output | tr -d '"'""",
         returnStdout: true
       ).trim()
       data['tf_workspace_path'] = sh (
