71846c74948cef187e563badfffb41912f1e8a3c
oviner <oviner@redhat.com>
2023-07-07 12:55:42 +0300

    123

--- vars/callTerraform.groovy
+++ vars/callTerraform.groovy
@@ -1,6 +1,16 @@
 def call(tf_workspace) {
+    data = [:] 
     dir(tf_workspace){
         sh "terraform init"
         sh "terraform apply --auto-approve"
+      data['ip_address'] = sh (
+        script: """terraform output ip_address | tr -d '"'""",
+        returnStdout: true
+      ).trim()
+      data['tf_workspace_path'] = sh (
+        script: "pwd",
+        returnStdout: true
+      ).trim()
+      println data
     }
 }
